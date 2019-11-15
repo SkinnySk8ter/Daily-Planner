d2bbf2fc16fd1f3408ef2ec5b97e93adf2a66335
Jody Jones <jodybjones@yahoo.com>
2019-11-15 01:39:04 -0500

    need to work on getting timer up and getting local storage to work when add button is clicked

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,3 +1,17 @@
 // add timeclock to top half into timeclock div space
+var date = moment().format('MMMM Do YYYY, h:mm:ss a');
+
+$("thedate").append(date); //need to get the clock to actually appear on the page
+
+
+
 // make the text boxes interactive with the time of day!! (should change colors to reflect past present and future) set up an .addeventlistener????
+
+
+
 // make the save button save the input text to local storage. 
+
+$("#save1").click(function(){
+window.localStorage.setItem("text1", JSON.stringify(""))
+
+})
