65395b1506869168006ec163f0762d60d7ed9ab9
Jody Jones <jodybjones@yahoo.com>
2019-11-15 01:02:58 -0500

    created notes on javascript page to hone in on tasks at hand

---
+++ assets/javascript.js
@@ -0,0 +1,3 @@
+// add timeclock to top half into timeclock div space
+// make the text boxes interactive with the time of day!! (should change colors to reflect past present and future) set up an .addeventlistener????
+// make the save button save the input text to local storage. 
