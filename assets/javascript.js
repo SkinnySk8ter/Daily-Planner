727e47e645792622d8e121f9f9241bb604b58ed2
Jody Jones <jodybjones@yahoo.com>
2019-11-16 22:27:11 -0500

    still ironing out page. Keep receiving error messages referencing to page.

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,29 +1,17 @@
-// add timeclock to top half into timeclock div space
-// date = theClock()
-
-// function theClock()
-// {
-// var date= document.getElementById("thedate").innerHTML ;
-// theClock(date);
-
-// }
-
-
-// var date = moment().format();
-
-
-// $("thedate").append(date); //need to get the clock to actually appear on the page
-
-
-
 // make the text boxes interactive with the time of day!! (should change colors to reflect past present and future) set up an .addeventlistener????
 
 
 
 // make the save button save the input text to local storage. 
 
-$("#save1").click(function(){
-    var clickthis = window.localStorage.setItem("text1", JSON.stringify(""))
-    clickthis()
+// var savethis = document.getElementById("save1")
+// function rememberthis(){
+//     var saveclick = document.getElementById("save1").addEventListener(onclick){
+        
+//     }
+// }
+// $("#save1").click(function(){
+//     var clickthis = window.localStorage.setItem("text1", JSON.stringify(""))
+//     clickthis()
 
-})
+// })
